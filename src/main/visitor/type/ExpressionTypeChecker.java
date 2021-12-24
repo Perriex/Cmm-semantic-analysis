3dfd59b0f391b334169169b5ab0ca7dff96795e8
pasadi <parna80as@gmail.com>
2021-12-24 15:10:01 +0330

    add

--- src/main/visitor/type/ExpressionTypeChecker.java
+++ src/main/visitor/type/ExpressionTypeChecker.java
@@ -5,70 +5,106 @@ import main.ast.nodes.expression.operators.BinaryOperator;
 import main.ast.nodes.expression.operators.UnaryOperator;
 import main.ast.nodes.expression.values.primitive.BoolValue;
 import main.ast.nodes.expression.values.primitive.IntValue;
-import main.ast.types.NoType;
-import main.ast.types.Type;
+import main.ast.types.*;
 import main.ast.types.primitives.BoolType;
 import main.ast.types.primitives.IntType;
-import main.compileError.typeError.UnsupportedOperandType;
+import main.ast.types.primitives.VoidType;
+import main.compileError.typeError.*;
+import main.symbolTable.SymbolTable;
+import main.symbolTable.exceptions.ItemNotFoundException;
+import main.symbolTable.items.StructSymbolTableItem;
+import main.symbolTable.items.SymbolTableItem;
+import main.symbolTable.items.VariableSymbolTableItem;
 import main.visitor.Visitor;
+
+import java.rmi.NoSuchObjectException;
 
 public class ExpressionTypeChecker extends Visitor<Type> {
 
-    private String getOperand(BinaryOperator type)
+    private Type checkIntTypeEx(BinaryExpression binaryExpression,Type type, BinaryOperator operator)
     {
-        if(type == BinaryOperator.eq)
-            return "eq";
-        if(type == BinaryOperator.gt)
-            return "gt";
-        if(type == BinaryOperator.lt)
-            return "lt";
-        if(type == BinaryOperator.add)
-            return "add";
-        if(type == BinaryOperator.sub)
-            return "sub";
-        if(type == BinaryOperator.mult)
-            return "mult";
-        if(type == BinaryOperator.div)
-            return "div";
-        if(type == BinaryOperator.and)
-            return "and";
-        if(type == BinaryOperator.or)
-            return "or";
-        if(type == BinaryOperator.assign)
-            return "assign";
-        return "";
-    }
-    @Override
-    public Type visit(BinaryExpression binaryExpression) {
-        //Todo
-        Expression l = binaryExpression.getFirstOperand();
-        Expression r = binaryExpression.getSecondOperand();
-        Type lType = l.accept(this);
-        Type rType = r.accept(this);
-        BinaryOperator operator = binaryExpression.getBinaryOperator();
-        if(operator == BinaryOperator.and || operator == BinaryOperator.or )
+        if(type instanceof IntType)
         {
-            if(lType instanceof BoolType && rType instanceof BoolType)
+            if ( operator != BinaryOperator.or && operator != BinaryOperator.and)
+            {
+                if(operator == BinaryOperator.assign)
+                    return new VoidType();
+                if(operator == BinaryOperator.sub || operator == BinaryOperator.div ||
+                    operator == BinaryOperator.mult || operator == BinaryOperator.add)
+                    return new IntType();
+                return new BoolType();
+            }
+        }
+        if(!(type instanceof NoType))
+        {
+            binaryExpression.addError(new UnsupportedOperandType(binaryExpression.getLine(),operator.name()));
+        }
+        return new NoType();
+    }
+    private Type checkBoolTypeEx(BinaryExpression binaryExpression,Type type, BinaryOperator operator)
+    {
+        if(type instanceof BoolType)
+        {
+            if ( operator == BinaryOperator.or || operator == BinaryOperator.and || operator == BinaryOperator.eq)
             {
                 return new BoolType();
             }
-            else if((lType instanceof IntType && rType instanceof NoType) ||
-                    (lType instanceof NoType && rType instanceof IntType) ||
-                    (lType instanceof IntType && rType instanceof IntType)){
-                binaryExpression.addError(new UnsupportedOperandType(binaryExpression.getLine(), getOperand(operator) ));
-                return new NoType();
+            if (operator == BinaryOperator.assign)
+            {
+                return new VoidType();
             }
         }
-        else{
-            if(lType instanceof IntType && rType instanceof IntType)
-            {
-                return new IntType();
-            }
-            else if((lType instanceof BoolType && rType instanceof NoType) ||
-                    (lType instanceof NoType && rType instanceof BoolType) ||
-                    (lType instanceof BoolType && rType instanceof BoolType)){
-                binaryExpression.addError(new UnsupportedOperandType(binaryExpression.getLine(),getOperand(operator) ));
-                return new NoType();
+        if(!(type instanceof NoType))
+        {
+            binaryExpression.addError(new UnsupportedOperandType(binaryExpression.getLine(),operator.name()));
+        }
+        return new NoType();
+    }
+
+    @Override
+    public Type visit(BinaryExpression binaryExpression) {
+        Type lType = binaryExpression.getFirstOperand().accept(this);
+        Type rType = binaryExpression.getSecondOperand().accept(this);
+        BinaryOperator operator = binaryExpression.getBinaryOperator();
+        if (lType instanceof IntType)
+        {
+            return checkIntTypeEx(binaryExpression,rType, operator);
+        }
+        else if (rType instanceof IntType)
+        {
+            return checkIntTypeEx(binaryExpression,lType, operator);
+        }
+        else if (lType instanceof BoolType)
+        {
+            return checkBoolTypeEx(binaryExpression,rType, operator);
+        }
+        else if (rType instanceof BoolType)
+        {
+            return checkBoolTypeEx(binaryExpression, lType, operator);
+        }
+        else
+        {
+            if(!(rType instanceof NoType && lType instanceof NoType)) {
+                if (operator == BinaryOperator.eq) {
+                    if ((rType instanceof FptrType && lType instanceof FptrType) ||
+                        (rType instanceof StructType && lType instanceof StructType)) {
+                        return new BoolType();
+                    }
+                    else
+                    {
+                        binaryExpression.addError(new UnsupportedOperandType(binaryExpression.getLine(),operator.name()));
+                    }
+                } else if (operator == BinaryOperator.assign) {
+                    if ((rType instanceof FptrType && lType instanceof FptrType) ||
+                       (rType instanceof StructType && lType instanceof StructType) ||
+                       (rType instanceof ListType && lType instanceof ListType)) {
+                        return new VoidType();
+                    }
+                    else
+                    {
+                        binaryExpression.addError(new UnsupportedOperandType(binaryExpression.getLine(),operator.name()));
+                    }
+                }
             }
         }
         return new NoType();
@@ -76,21 +112,19 @@ public class ExpressionTypeChecker extends Visitor<Type> {
 
     @Override
     public Type visit(UnaryExpression unaryExpression) {
-        //Todo
-        Expression ex = unaryExpression.getOperand();
-        Type exType = ex.accept(this);
-        if(unaryExpression.getOperator() == UnaryOperator.not)
+        Type exType =  unaryExpression.getOperand().accept(this);
+        if (unaryExpression.getOperator() == UnaryOperator.not)
         {
-            if(exType instanceof BoolType)
+            if (exType instanceof BoolType)
             {
                 return new BoolType();
             }
             else if(exType instanceof IntType)
             {
                 unaryExpression.addError(new UnsupportedOperandType(unaryExpression.getLine(), "not"));
-                return new NoType();
             }
-        }else
+        }
+        else
         {
             if(exType instanceof IntType)
             {
@@ -99,7 +133,6 @@ public class ExpressionTypeChecker extends Visitor<Type> {
             else if(exType instanceof BoolType)
             {
                 unaryExpression.addError(new UnsupportedOperandType(unaryExpression.getLine(), "minus"));
-                return new NoType();
             }
         }
         return new NoType();
@@ -113,20 +146,58 @@ public class ExpressionTypeChecker extends Visitor<Type> {
 
     @Override
     public Type visit(Identifier identifier) {
-        //Todo
+
         return null;
     }
 
     @Override
     public Type visit(ListAccessByIndex listAccessByIndex) {
-        //Todo
-        return null;
+        Type instType = listAccessByIndex.getInstance().accept(this);
+        Type indexType = listAccessByIndex.getIndex().accept(this);
+        if (instType instanceof ListType && indexType instanceof IntType)
+        {
+            return ((ListType) instType).getType();
+        }
+        else if (instType instanceof ListType && !(indexType instanceof NoType))
+        {
+            listAccessByIndex.addError(new ListIndexNotInt(listAccessByIndex.getLine()));
+        }
+        else if (!(instType instanceof NoType) && indexType instanceof IntType)
+        {
+           listAccessByIndex.addError(new AccessByIndexOnNonList(listAccessByIndex.getLine()));
+        }
+        return new NoType();
     }
 
     @Override
     public Type visit(StructAccess structAccess) {
-        //Todo
-        return null;
+        Type instType = structAccess.getInstance().accept(this);
+        if (!(instType instanceof StructType))
+        {
+            structAccess.addError(new AccessOnNonStruct(structAccess.getLine()));
+            return new NoType();
+        }
+        String varName = structAccess.getElement().getName();
+        String structName = ((StructType) instType).getStructName().getName();
+        try
+        {
+            StructSymbolTableItem struct = (StructSymbolTableItem) SymbolTable.top.getItem(structName);
+            SymbolTable structTable = struct.getStructSymbolTable();
+            try
+            {
+                VariableSymbolTableItem element = (VariableSymbolTableItem) structTable.getItem(varName);
+                return element.getType();
+            }
+            catch (ItemNotFoundException ex)
+            {
+                structAccess.addError(new StructMemberNotFound(structAccess.getLine(),structName,varName));
+                return new NoType();
+            }
+        }
+        catch (ItemNotFoundException ex)
+        {
+            return new NoType();
+        }
     }
 
     @Override
@@ -137,25 +208,40 @@ public class ExpressionTypeChecker extends Visitor<Type> {
 
     @Override
     public Type visit(ListAppend listAppend) {
-        //Todo
-        return null;
+        Type listType = listAppend.getListArg().accept(this);
+        if(!(listType instanceof ListType))
+        {
+            listAppend.addError(new AppendToNonList(listAppend.getLine()));
+            return new NoType();
+        }
+        Type listEl = listAppend.getElementArg().accept(this);
+        Type listEls = ((ListType) listType).getType();
+        if((listEl instanceof BoolType && listEls instanceof BoolType ) ||
+         (listEl instanceof IntType && listEls instanceof IntType ) ||
+         (listEl instanceof StructType && listEls instanceof StructType ) ||
+         (listEl instanceof ListType && listEls instanceof ListType ) )
+        {
+            return new VoidType();
+        }
+        if(!(listEls instanceof NoType))
+        {
+            listAppend.addError(new NewElementTypeNotMatchListType(listAppend.getLine()));
+        }
+        return new NoType();
     }
 
     @Override
     public Type visit(ExprInPar exprInPar) {
-        //Todo
-        return null;
+        return exprInPar.getInputs().get(0).accept(this);
     }
 
     @Override
     public Type visit(IntValue intValue) {
-        //Todo
-        return null;
+        return new IntType();
     }
 
     @Override
     public Type visit(BoolValue boolValue) {
-        //Todo
-        return null;
+        return new BoolType();
     }
 }
